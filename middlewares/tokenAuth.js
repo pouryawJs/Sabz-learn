b1ffaf59ed471c1651bc284e93aca010290ca40c
pouryawJs <pourya.bu@gmail.com>
2023-09-04 20:04:23 +0430

    Limiting the use of user banning by isAdmin middleware and authtoken middleware

---
+++ middlewares/tokenAuth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken")
+const userModel = require("./../models/User")
+
+module.exports = async (req, res, next) => {
+    
+    const authHeader = req.header("Authorization")?.split(" ")
+
+    if(authHeader?.length !== 2){
+        return res.status(403).json({ message : "you cant have access to this api"})
+    }
+
+    const token = authHeader[1]
+    
+    const payloadData = jwt.verify(token, process.env.JWT_SECRET)
+
+    try {
+        
+        const user = await userModel.findById(payloadData.id)
+        Reflect.deleteProperty(user, "password")
+        req.user = user
+
+        return next()
+
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+
+}
--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const userController = require("./../../controllers/v1/userController")
+const isAdmin = require("./../../middlewares/isAdmin")
+const tokenAuth = require("./../../middlewares/tokenAuth")
 
 const router = express.Router()
 
-router.post("/ban/:id", userController.ban)
+router
+    .route("/ban/:id")
+    .post(tokenAuth, isAdmin, userController.ban)
 
 module.exports = router
