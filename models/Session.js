9b58e8f734d13156f710e9ed2b0000b46a055b2e
pouryawJs <pourya.bu@gmail.com>
2023-09-10 14:36:27 +0430

    create Session model

---
+++ models/Session.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const schema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    time : {
+        type : String,
+        required : true
+    },
+    free : {
+        type : Nunber,
+        required : true
+    },
+    video : {
+        type : String,
+        required : true
+    },
+    course: {
+        type: mongoose.Types.ObjectId,
+        ref: "course"
+    }
+}, { timestamps: true, versionKey: false })
+
+const model = mongoose.model("session", schema)
+
+module.exports = model
